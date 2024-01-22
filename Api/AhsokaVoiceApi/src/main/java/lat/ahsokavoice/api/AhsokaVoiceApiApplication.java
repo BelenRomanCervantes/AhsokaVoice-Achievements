06e7e9002caa479400f04a1f06551e60ddadd11f
BelenRoman <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-01-22 14:44:15 -0600

    se añade carpeta de Api creada por Belen

---
+++ Api/AhsokaVoiceApi/src/main/java/lat/ahsokavoice/api/AhsokaVoiceApiApplication.java
@@ -0,0 +1,13 @@
+package lat.ahsokavoice.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsokaVoiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhsokaVoiceApiApplication.class, args);
+    }
+
+}
