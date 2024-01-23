bdb264b39b72da591c16c8836e94fc7ff6c60cf9
BelenRoman <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-01-22 18:17:42 -0600

    Primer commit

--- Api/AhsokaVoiceApi/src/main/java/lat/ahsokavoice/api/AhsokaVoiceApiApplication.java
+++ Api/AhsokaVoiceApi/src/main/java/lat/ahsokavoice/api/AhsokaVoiceApiApplication.java
@@ -1,13 +1,21 @@
 package lat.ahsokavoice.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AhsokaVoiceApiApplication {
+public class AhsokaVoiceApiApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AhsokaVoiceApiApplication.class, args);
     }
 
+    @Value("${value.prueba}")
+    private String helloWorld;
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(helloWorld);
+    }
 }
