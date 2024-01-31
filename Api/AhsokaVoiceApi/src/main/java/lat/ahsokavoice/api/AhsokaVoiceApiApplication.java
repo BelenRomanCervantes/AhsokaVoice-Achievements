2a759385a0deda03bad961ee481af1c525720306
BelenRoman <124020263+BelenRomanCervantes@users.noreply.github.com>
2024-01-30 19:00:18 -0600

    Edité ApiApplication para que funcionara el perfil de prod pero aún no influye en la base de datos de PGAdmin

--- Api/AhsokaVoiceApi/src/main/java/lat/ahsokavoice/api/AhsokaVoiceApiApplication.java
+++ Api/AhsokaVoiceApi/src/main/java/lat/ahsokavoice/api/AhsokaVoiceApiApplication.java
@@ -1,21 +1,13 @@
 package lat.ahsokavoice.api;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AhsokaVoiceApiApplication implements CommandLineRunner {
+public class AhsokaVoiceApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AhsokaVoiceApiApplication.class, args);
     }
 
-    @Value("${value.prueba}")
-    private String helloWorld;
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(helloWorld);
-    }
 }
